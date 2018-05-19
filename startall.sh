e27f76054c1e0476d65cb4aa1e67ff2cfd68d070
dmitriy <workday2006@gmail.com>
2018-05-19 21:07:43 +0300

    init

--- startall.sh
+++ startall.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")"
+
 chmod +x ./stop.sh
 ./stop.sh
 
 mvn clean install
+mvn -f ../entity clean install
+mvn -f ../identity clean install
+mvn -f ../gateway clean install
+mvn -f ../request clean install
+mvn -f ../persist clean install
 
-chmod +x ./eureka/target/eureka-1.0-SNAPSHOT.jar
-chmod +x ../gateway/target/gateway-1.0-SNAPSHOT.jar
-chmod +x ../identity/target/identity-1.0-SNAPSHOT.jar
-chmod +x ../request/target/request-1.0-SNAPSHOT.jar
-chmod +x ../persist/target/persist-1.0-SNAPSHOT.jar
+chmod +x ./target/fxquotes.eureka-1.0-SNAPSHOT.jar
+chmod +x ../gateway/target/fxquotes.gateway-1.0-SNAPSHOT.jar
+chmod +x ../identity/target/fxquotes.identity-1.0-SNAPSHOT.jar
+chmod +x ../request/target/fxquotes.request-1.0-SNAPSHOT.jar
+chmod +x ../persist/target/fxquotes.persist-1.0-SNAPSHOT.jar
 
-nohup java -jar ../eureka/target/*.jar &
+nohup java -jar ./target/*.jar &
 nohup java -jar ../gateway/target/*.jar &
 nohup java -jar ../identity/target/*.jar &
 nohup java -jar ../request/target/*.jar &
 nohup java -jar ../persist/target/*.jar &
 
 #run angular
-cd ../angular/
+cd ../front/
 ng build --prod
 ng server --open
