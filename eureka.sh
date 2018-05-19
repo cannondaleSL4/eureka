996cd6e17338b1e6a3a305117ee551e913baacc6
dmitriy <workday2006@gmail.com>
2018-05-20 00:16:53 +0300

    init

--- eureka.sh
+++ eureka.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 fuser -k 8761/tcp
-cd ../eureka
-#mvn clean install
+
+cd "$(dirname "$0")"
+
+mvn clean install
 
 chmod +x ./target/fxquotes.eureka-1.0-SNAPSHOT.jar
 java -jar ./target/*.jar
